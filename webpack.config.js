10a092884c15b2d5be8a9fd18b09f8acaa42732d
Alexander Glintschert <alexander.glintschert@asideas.de>
2020-06-10 21:26:42 +0200

    move images to src directory; upgrade some dependencies ensure installation of images

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,44 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/AddToHomeScreen.js',
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'AddToHomeScreen.js',
-        library: 'add-to-homescreen-react',
-        libraryTarget: 'umd'
-    },
-    externals: [nodeExternals()],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                include: path.resolve(__dirname, 'src'),
-                use: ['babel-loader']
-            },
-            {
-                test: /\.scss$/,
-                include: path.resolve(__dirname, 'src'),
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
+  entry: './src/AddToHomeScreen.js',
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'AddToHomeScreen.js',
+    library: 'add-to-homescreen-react',
+    libraryTarget: 'umd'
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
         ]
-    }
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{
+        context: 'src/images',
+        from: '**/*',
+        to: path.resolve(__dirname, 'build/images')
+      }]
+    })
+  ]
 };
