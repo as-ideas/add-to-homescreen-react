b64f3fc0e36fe98908affcd67c8babec4371ef9f
liza-shcher <46323660+liza-shcher@users.noreply.github.com>
2021-02-22 15:03:31 +0000

    Deploying to gh-pages from @ as-ideas/add-to-homescreen-react@6748542d6d97f73aa96697c81a4cd5f1369ee335 🚀

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
-
-const container = document.createElement('div');
-document.body.appendChild(container);
-ReactDOM.render(<App/>, container);
